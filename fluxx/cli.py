915feac2b43fcc2ee771d76b4912934dec004f2a
Connor Sullivan <sully4792@gmail.com>
2018-03-29 18:05:01 -0400

    added methods to create, update, and delete

--- fluxx/cli.py
+++ fluxx/cli.py
@@ -5,6 +5,8 @@ import threading
 import queue
 import json
 from datetime import datetime
+from contextlib import contextmanager
+from functools import partialmethod
 
 import fire
 
@@ -14,8 +16,24 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 DEFAULT_LOG_PATH = './logs'
-DEFAULT_THREAD_COUNT = 12
+DEFAULT_THREAD_COUNT = 5
 DEFAULT_PER_PAGE = 100
+
+
+@contextmanager
+def write_operation(instance, model, threads):
+    "Initialize queue, read input, start and end threads."
+    q = queue.Queue()
+    json_data = sys.stdin.read()
+    input_data = json.loads(json_data)
+    yield (q, input_data)
+
+    for _ in range(threads):
+        worker = FluxxThread(q, instance, model)
+        worker.daemon = True
+        worker.start()
+
+    q.join()
 
 
 class FluxxThread(threading.Thread):
@@ -33,21 +51,28 @@ class FluxxThread(threading.Thread):
     def run(self):
         while True:
             item = self.q.get()
+            rec_id = item.pop('id', None)
+            rec_method = item.get('method').upper()
 
             try:
-                rec_id = item.get('id')
-                if rec_id:
-                    updated = self.client.update(self.model, rec_id, item)
-                    log.info('Updated {}'.format(updated['id']))
-                else:
+                if rec_method == 'CREATE':
                     created = self.client.create(self.model, item)
-                    log.info('Created {}'.format(created['id']))
+                    log.info('Created %s', created['id'])
 
+                elif rec_method == 'UPDATE':
+                    updated = self.client.update(self.model, rec_id, item)
+                    log.info('Updated %s', updated['id'])
+
+                elif rec_method == 'DELETE':
+                    deleted = self.client.delete(self.model, rec_id)
+                    log.info('Deleted %s', deleted['id'])
+                else:
+                    log.error('Method not specified')
             except NotImplementedError:
                 log.error('Process method not implemented.')
 
-            except fluxx.FluxxError as e:
-                log.error(e)
+            except fluxx.FluxxError as error:
+                log.error(error)
 
             finally:
                 self.q.task_done()
@@ -65,30 +90,6 @@ class FluxxCLI(object):
         handler = logging.FileHandler(log_file)
         log.addHandler(handler)
 
-    def crud(self, model, threads=DEFAULT_THREAD_COUNT):
-        """Creates or updates a each record provided in the list.
-        The non-null status of the 'id' attribute of every record determines
-        whether it will be created or updated, with None value IDs defaulting
-        to creation.
-
-        :model: The Fluxx ModelObject you wish to create.
-        :returns: None
-
-        """
-
-        q = queue.Queue()
-
-        input_data = sys.stdin.read()
-        for i, record in enumerate(json.loads(input_data)):
-            q.put(record)
-
-        for _ in range(threads):
-            worker = FluxxThread(q, self.instance, model)
-            worker.daemon = True
-            worker.start()
-
-        q.join()
-
     def list(self, model, cols, page=1, per_page=DEFAULT_PER_PAGE):
         """Return a list of records according to the Page and PerPage
         settings. Page must be greater than 0.
@@ -104,6 +105,44 @@ class FluxxCLI(object):
         records = client.list(model, cols=list(cols), page=page, per_page=per_page)
 
         sys.stdout.write(str(json.dumps(records)))
+
+    def write(self, method, model, threads=DEFAULT_THREAD_COUNT):
+        """Updates each record provided in the list.
+        Each record must have an id.
+
+        :model: The Fluxx ModelObject you wish to create.
+        :returns: None
+
+        """
+
+        with write_operation(self.instance, model, threads) as (q, records):
+            for i, record in enumerate(records):
+                record['method'] = method.upper()
+                q.put(record)
+
+    create = partialmethod(write, 'CREATE')
+    update = partialmethod(write, 'UPDATE')
+    delete = partialmethod(write, 'DELETE')
+
+    def crup(self, model, threads=DEFAULT_THREAD_COUNT):
+        """Creates or updates a each record provided in the list.
+        The non-null status of the 'id' attribute of every record determines
+        whether it will be created or updated, with None value IDs defaulting
+        to creation.
+
+        :model: The Fluxx ModelObject you wish to create.
+        :returns: None
+
+        """
+
+        with write_operation(self.instance, model, threads) as (q, records):
+            for i, record in enumerate(records):
+                if 'id' in record:
+                    record['method'] = 'UPDATE'
+                else:
+                    record['method'] = 'CREATE'
+
+                q.put(record)
 
 
 def main():
